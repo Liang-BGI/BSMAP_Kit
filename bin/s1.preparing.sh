0b25d8ba7517f70d5a38ed50b35321a7182edbf1
Hao Yu <yuhao819@hotmail.com>
2017-11-23 17:56:22 +0800

    Delete 4 runtime files
    Delete 4 runtime files
    
    These files would be generated by the main script

--- bin/s1.preparing.sh
+++
@@ -1,2 +0,0 @@
-echo "job start at: $(date +%Y-%m-%d:%H:%M:%S)" && cat ../example/data/ref_genome/sample.genome.fa.gz | perl /usr/local/util/fasta2methBaseC.FT.pl > ../example/data/ref_genome/sample.genome.fa.gz.methBaseC.gz && echo "job end at: $(date +%Y-%m-%d:%H:%M:%S)"
-
--- bin/s4.cleaning.sh
+++
@@ -1,5 +0,0 @@
-echo "cleaning temporary files ..." && rm -f ./output/sample1/rep2/lib2/lane1_lib2_L01/3/lane1_lib2_L01_3.methBaseC ./output/sample1/rep2/lib2/lane1_lib2_L01/3/lane1_lib2_L01_3.binTest.methBaseC.gz && echo "cleaning is accomplished"
-echo "cleaning temporary files ..." && rm -f ./output/sample1/rep2/lib2/lane1_lib2_L01/4/lane1_lib2_L01_4.methBaseC ./output/sample1/rep2/lib2/lane1_lib2_L01/4/lane1_lib2_L01_4.binTest.methBaseC.gz && echo "cleaning is accomplished"
-echo "cleaning temporary files ..." && rm -f ./output/sample1/rep1/lib1/lane1_lib1_L01/1/lane1_lib1_L01_1.methBaseC ./output/sample1/rep1/lib1/lane1_lib1_L01/1/lane1_lib1_L01_1.binTest.methBaseC.gz && echo "cleaning is accomplished"
-echo "cleaning temporary files ..." && rm -f ./output/sample1/rep1/lib1/lane1_lib1_L01/2/lane1_lib1_L01_2.methBaseC ./output/sample1/rep1/lib1/lane1_lib1_L01/2/lane1_lib1_L01_2.binTest.methBaseC.gz && echo "cleaning is accomplished"
-
